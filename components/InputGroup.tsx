7747b62d4b28c54cff1681d894f90e7610b5ae2f
mama4294 <matthew.malone@colorado.edu>
2023-03-14 22:02:12 -0600

    creates table grid

---
+++ components/InputGroup.tsx
@@ -0,0 +1,39 @@
+type Props = {
+  label: string;
+  value: number;
+  placeholder: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  unit: string;
+};
+
+export const InputGroup = ({
+  label,
+  placeholder,
+  value,
+  onChange,
+  unit,
+}: Props) => {
+  return (
+    <div>
+      <label
+        htmlFor={label}
+        className="block mb-2 text-sm font-medium text-gray-900 "
+      >
+        {label}
+      </label>
+      <div className="flex">
+        <input
+          type="text"
+          id={label}
+          className="bg-gray-50 border rounded-none rounded-l-lg border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+        <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-gray-300 rounded-r-lg border-l-0 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+          {unit}
+        </span>
+      </div>
+    </div>
+  );
+};
--- components/Stage.tsx
+++ components/Stage.tsx
@@ -1,5 +1,5 @@
 import { useReducer } from "react";
-import Canvas from "./Canvas";
+import { InputGroup } from "./InputGroup";
 
 export const Stage = () => {
   type State = {
@@ -39,85 +39,64 @@ export const Stage = () => {
 
   return (
     <div>
-      {/* <Canvas /> */}
-      <Grid length={state.planterLength} width={state.planterWidth} />
-      <div className="grid gap-6 mb-6 md:grid-cols-2 text-left text-sm font-medium text-gray-900 dark:text-white">
-        <div>
-          <label
-            htmlFor="first_name"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Planter Width
-          </label>
-          <div className="flex">
-            <input
-              type="text"
-              id="first_name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="10"
-              value={state.planterWidth}
-              onChange={(e) =>
-                dispatch({
-                  type: ActionKind.CHANGE_WIDTH,
-                  payload: Number(e.target.value),
-                })
-              }
-            />
-            <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-gray-300 rounded-r-lg border-l-0 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-              inches
-            </span>
-          </div>
-        </div>
-        <div>
-          <label
-            htmlFor="first_name"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Planter Length
-          </label>
-          <div className="flex">
-            <input
-              type="text"
-              id="first_name"
-              className="bg-gray-50 border rounded-none rounded-l-lg border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="10"
-              value={state.planterLength}
-              onChange={(e) =>
-                dispatch({
-                  type: ActionKind.CHANGE_LENGTH,
-                  payload: Number(e.target.value),
-                })
-              }
-            />
-            <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-gray-300 rounded-r-lg border-l-0 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-              inches
-            </span>
-          </div>
-        </div>
+      <div className="grid gap-6 mb-6 md:grid-cols-2 text-left text-sm font-medium text-gray-900 dark:text-white my-4">
+        <InputGroup
+          label="Planter Length"
+          placeholder="10"
+          value={state.planterLength}
+          unit="inches"
+          onChange={(e) =>
+            dispatch({
+              type: ActionKind.CHANGE_LENGTH,
+              payload: Number(e.target.value),
+            })
+          }
+        />
+        <InputGroup
+          label="Planter Width"
+          placeholder="10"
+          value={state.planterWidth}
+          unit="inches"
+          onChange={(e) =>
+            dispatch({
+              type: ActionKind.CHANGE_WIDTH,
+              payload: Number(e.target.value),
+            })
+          }
+        />
+      </div>
+      <GridTable length={state.planterLength} width={state.planterWidth} />
+    </div>
+  );
+};
+
+const GridTable = ({ length, width }: { length: number; width: number }) => {
+  const generateArray = (width: number) => {
+    return [...Array(width)].map((_, x) => 0);
+  };
+
+  const generateArrayOfArrays = (length: number, width: number) => {
+    return [...Array(length)].map((_, y) => generateArray(width));
+  };
+
+  const gridData = generateArrayOfArrays(length, width);
+  console.table(gridData);
+
+  return (
+    <div className="flex justify-center items-center">
+      <div className=" border-gray-500 p-2 border-4 rounded">
+        <table className="table-fixed border-gray-500 border-2 rounded">
+          <tbody>
+            {gridData.map((row, y) => (
+              <tr>
+                {row.map((col, x) => (
+                  <td className="border border-gray-200 h-4 w-4 hover:bg-yellow-200"></td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
-};
-
-const Grid = ({ length, width }: { length: number; width: number }) => {
-  const numsquares = length * width;
-  return (
-    <div className="grid grid-cols-10 mb-10">
-      {[...Array(length)].map((_, y) => (
-        <>
-          {[...Array(width)].map((_, x) => (
-            <Square label={`${x},${y}`} />
-          ))}
-        </>
-      ))}
-
-      {/* {[...Array(numsquares)].map((_, i) => (
-        <Square label={i.toString()} />
-      ))} */}
-    </div>
-  );
-};
-
-const Square = ({ label }: { label: string }) => {
-  return <p className="bg-gray-100 p-2 hover:bg-yellow-300">{label}</p>;
 };
