99e1aa94e65a3888dcf2bb4eb085969bd1a3c7df
mama4294 <matthew.malone@colorado.edu>
2023-03-15 20:10:48 -0600

    adds DaisyUi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
+  plugins: [require("daisyui")],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+};
