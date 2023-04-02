dbd523a8414ed0619e78f9b011b112977d4be3fb
mama4294 <matthew.malone@colorado.edu>
2023-04-02 11:24:45 -0600

    changes select style

--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import {
+  Cog6ToothIcon,
   CursorArrowRaysIcon,
   PlusIcon,
   TrashIcon,
-} from "@heroicons/react/24/outline";
+} from "@heroicons/react/24/solid";
 import { useState } from "react";
 import Select, { SingleValue } from "react-select";
 import Konva from "../../components/Konva";
@@ -25,7 +26,7 @@ function EditPage() {
   ];
 
   const [selectedPlant, setSelectedPlant] = useState<Plant>(options[0]);
-  const [showDimentions, setShowDimentions] = useState<boolean>(false);
+  const [showDimentions, setShowDimentions] = useState<boolean>(true);
   const [mode, setMode] = useState(MODE.ADD);
 
   const changeSelectedPlant = (e: SingleValue<Plant>) => {
@@ -38,10 +39,25 @@ function EditPage() {
     });
   };
 
-  return (
-    <div className="flex flex-col gap-4 m-4 md:flex-row">
-      <div className="bg-base-200 p-4 rounded-lg shadow-md">
-        <div className="flex gap-2 justify-between items-center md:flex-col ">
+  const Menu = () => {
+    const customSelectStyles = {
+      control: (provided: any, state: any) => ({
+        ...provided,
+        backgroundColor: mode == MODE.ADD ? `hsl(var(--p))` : "inherit",
+
+        // "&:hover": {
+        //   backgroundColor: `hsl(var(--bc)`,
+        // },
+      }),
+      singleValue: (provided: any) => ({
+        ...provided,
+        color: mode == MODE.ADD ? `hsl(var(--pc))` : "",
+      }),
+    };
+
+    return (
+      <header className="bg-base-200 p-4 rounded-lg shadow-md flex items-center justify-between">
+        <div className="flex items-center gap-2">
           <button
             className={`btn btn-sm  ${
               mode == MODE.SELECT ? "btn-primary" : "btn-ghost"
@@ -50,40 +66,49 @@ function EditPage() {
           >
             <CursorArrowRaysIcon className="h-6 w-6" />
           </button>
-          <button
-            className={`btn btn-sm  ${
-              mode == MODE.ADD ? "btn-primary" : "btn-ghost"
-            }`}
-            onClick={() => setMode(MODE.ADD)}
-          >
-            <PlusIcon className="h-6 w-6" />
-          </button>
-          <button
-            className="btn btn-sm btn-ghost"
-            onClick={() => console.log("delete")}
-          >
-            <TrashIcon className="h-6 w-6" />
-          </button>
           <Select
+            className="bg-base-200 fill-base-content"
             options={options}
             onChange={changeSelectedPlant}
             value={selectedPlant}
+            styles={customSelectStyles}
           />
-          <div className="form-control">
-            <label className="label cursor-pointer">
-              <span className="label-text">Show Dimentions</span>
-              <input
-                type="checkbox"
-                className="toggle"
-                checked={showDimentions}
-                onChange={() => setShowDimentions((prev) => !prev)}
-              />
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="dropdown dropdown-bottom dropdown-end">
+            <label tabIndex={0} className="btn m-1 btn-sm btn-ghost">
+              <Cog6ToothIcon className="h-6 w-6" />
             </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <div className="form-control">
+                  <label className="label cursor-pointer">
+                    <span className="label-text">Show Dimentions</span>
+                    <input
+                      type="checkbox"
+                      className="toggle"
+                      checked={showDimentions}
+                      onChange={() => setShowDimentions((prev) => !prev)}
+                    />
+                  </label>
+                </div>
+              </li>
+            </ul>
           </div>
+
           <button className="btn btn-sm btn-primary">Save</button>
         </div>
-      </div>
-      <div className="bg-base-200 p-4 rounded-lg flex-1 shadow-md">
+      </header>
+    );
+  };
+
+  return (
+    <div>
+      <Menu />
+      <div className="bg-base-200 p-4 rounded-lg flex-1 shadow-md m-4">
         <Konva
           selectedPlant={selectedPlant}
           mode={mode}
