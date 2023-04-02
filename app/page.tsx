dfc9b32a29365201f65241972d395e921c277dd3
mama4294 <matthew.malone@colorado.edu>
2023-04-02 14:13:50 -0600

    New slogan

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ function HomePage() {
         <h1 className="text-6xl font-bold">
           Garden <p className="text-primary inline-block">Designer</p>
         </h1>
-        <p className="mt-3 text-2xl mb-10">Map out your planters</p>
+        <p className="mt-3 text-2xl mb-10">Plan your planters</p>
         <Link className="btn btn-primary gap-2" href="/planters/">
           <PlusCircleIcon className="h-8 w-8" />
           Get Started
