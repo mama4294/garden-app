f21d688bcb987d2851ebe4ef5721ce271d0dcece
mama4294 <matthew.malone@colorado.edu>
2023-04-28 22:15:35 -0600

    adds rename planter function

--- app/planters/PlanterMobileItem.tsx
+++ app/planters/PlanterMobileItem.tsx
@@ -1,26 +1,21 @@
 "use client";
 
 import {
-  ArrowDownLeftIcon,
-  ArrowLeftIcon,
   ArrowRightIcon,
   CheckIcon,
   PencilIcon,
   TrashIcon,
   XCircleIcon,
 } from "@heroicons/react/24/outline";
-import { collection, doc, orderBy, query } from "firebase/firestore";
+import { collection, doc, orderBy, query, updateDoc } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-import { useState } from "react";
-import {
-  useCollection,
-  useCollectionData,
-  useDocumentData,
-} from "react-firebase-hooks/firestore";
+import { ChangeEvent, useState } from "react";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 import { PlanterPreview } from "../../components/Konva";
 import Loading from "../../components/Loading";
 import { db } from "../../firebase";
+import NewPlanterModal from "./NewPlanterModal";
 
 export const PlanterMobileList = () => {
   const { data: session } = useSession();
@@ -38,6 +33,7 @@ export const PlanterMobileList = () => {
 
   return (
     <div className="flex gap-6 flex-col p-5 w-full h-full items-center overflow-auto">
+      <NewPlanterModal />
       {planters.map((data) => {
         const state = {
           name: data.name,
@@ -53,6 +49,7 @@ export const PlanterMobileList = () => {
 };
 
 const PlanterMobileItem = ({ state }: { state: Planter }) => {
+  const { data: session } = useSession();
   const [isEditing, setIsEditing] = useState(false);
   const [title, setTitle] = useState(state.name);
 
@@ -61,22 +58,33 @@ const PlanterMobileItem = ({ state }: { state: Planter }) => {
     setIsEditing(false);
   };
 
+  const handleRename = () => {
+    setIsEditing(false);
+    try {
+      updateDoc(doc(db, "users", session?.user?.email!, "planters", state.id), {
+        name: title,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="card card-compact w-full bg-base-200 shadow-xl">
       <div className="card-body">
         <div className="flex justify-between">
-          {!isEditing && <h2 className="card-title"> {state.name}</h2>}
-          {isEditing && (
-            <input
-              type="text"
-              placeholder="Planter Title"
-              className="input w-full max-w-xs card-title ml-0 pl-0"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          )}
+          <input
+            type="text"
+            disabled={!isEditing}
+            placeholder="Planter Title"
+            className="input w-full card-title mr-4 disabled:cursor-auto"
+            value={title}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setTitle(e.target.value)
+            }
+          />
 
-          <div className="flex">
+          <div className="flex gap-1">
             {!isEditing && (
               <button
                 className="btn btn-ghost p-1"
@@ -87,14 +95,11 @@ const PlanterMobileItem = ({ state }: { state: Planter }) => {
             )}
             {isEditing && (
               <>
+                <button className="btn btn-ghost p-1" onClick={handleRename}>
+                  <CheckIcon className="w-6 h-6" />
+                </button>
                 <button className="btn btn-ghost p-1" onClick={handleClear}>
                   <XCircleIcon className="w-6 h-6" />
-                </button>
-                <button
-                  className="btn btn-ghost p-1"
-                  onClick={() => setIsEditing(false)}
-                >
-                  <CheckIcon className="w-6 h-6" />
                 </button>
               </>
             )}
